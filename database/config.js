35dfd5be0c4692d7301294bcbd7f1e2a547de443
chibuezedev <paulpadro03@gmail.com>
2024-11-23 13:02:35 +0100

    update

--- database/config.js
+++ database/config.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 
 const connectWithDB = () => {
     
-  mongoose.connect(process.env.MONGODB_URI, {
+  mongoose.connect("mongodb+srv://Paul:y9DSqyzD8uiQ9n8g@node-class.iz8y6zp.mongodb.net/TonYield", {
   })
     .then(() => {
 
